6290048eac33457ea1a02cc171e4be59e0b28e30
kesav-venkatesan <kesav.officialmail@gmail.com>
2025-10-18 16:51:49 +0530

    feat:Search Function

--- contacts/models.py
+++ contacts/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Contact(models.Model):
+    name= models.CharField(max_length=100)
+    email=models.EmailField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="contacts")
+    
+    class Meta:
+        unique_together=('name','email')
+    def __str__(self):
+        return f'{self.name} {self.email}'
+    
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 # Create your views here.
+
+@login_required
 def index(request):
-    context = {}
+    contacts=request.user.contacts.order_by('-created_at')
+    context = {'contacts':contacts}
     return render(request, 'contacts.html', context)
+
+@login_required
+def search_view(request):
+    query = request.GET.get('search','')
+    
+    contacts = request.user.contacts.filter(Q(name__icontains=query) | Q(email__icontains=query))
+    
+    return render(request,'partials/contact-list.html',{'contacts':contacts})
